956a5a01fa2002244f97d6e11778636b7dba2b0e
Ji Young Lee <jiyle@microsoft.com>
2017-12-26 09:26:49 +0900

    12/26 Test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,9 +12,6 @@ namespace ConsoleApp2
         {
             Console.WriteLine("test");
             Console.WriteLine("test2");
-            Console.WriteLine("test3");
-            Console.WriteLine("test4");
-            Console.WriteLine("test5");
         }
     }
 }
